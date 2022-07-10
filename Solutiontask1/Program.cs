bb88da311a32f30b3f67f02518c36348083fac8f
Anton <seemannRR@ya.ru>
2022-07-10 22:25:24 +0300

    add solution1

---
+++ Solutiontask1/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputA = Console.ReadLine();
+string? inputB = Console.ReadLine();
+
+if(inputA != null && inputB != null)
+{
+    int inputnumberA = int.Parse (inputA);
+    int inputnumberB = int.Parse (inputB);
+
+    if(inputnumberA == Math.Sqrt(inputnumberB))
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+       Console.WriteLine("NO");
+    } 
+}
+    
+
+   
