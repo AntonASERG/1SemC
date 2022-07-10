6d10bfb360b99b4d31d81e95f849fe2cd15f6f2d
Anton <seemannRR@ya.ru>
2022-07-10 23:02:53 +0300

    add solution2

---
+++ Solutiontask2/Program.cs
@@ -0,0 +1,23 @@
+﻿string? inputline = Console.ReadLine();
+
+if(inputline != null)
+{
+    int inputnumber = int.Parse (inputline);
+    
+    string [] dayOfWeek = new string [7];
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Шаббат";
+    dayOfWeek[6] = "Воскресенье";
+
+// switch (inputnumber)
+//{
+    //case 1....7:
+    //Console.WriteLine("Понедельник")
+    //break;
+//}
+    Console.WriteLine(dayOfWeek[inputnumber-1]);
+}
