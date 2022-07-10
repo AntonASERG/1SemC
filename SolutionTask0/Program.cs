e2be9fcc9f6b6a54f836b4dab2d73d0b4ac26b92
Anton <seemannRR@ya.ru>
2022-07-10 21:38:42 +0300

    add solution 0

---
+++ SolutionTask0/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputline = Console.ReadLine();
+
+if(inputline != null)
+{
+    int inputnumber = int.Parse (inputline);
+    
+    //int outnumber = inputnumber*inputnumber;
+    int outnumber = (int)Math.Pow(inputnumber,2); 
+
+    Console.WriteLine(outnumber);
+}
