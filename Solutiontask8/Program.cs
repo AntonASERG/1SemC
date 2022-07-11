6af490840148bebd4aadcc155b4f933ac1e25aa5
Anton <seemannRR@ya.ru>
2022-07-11 22:01:46 +0300

    add Task8

---
+++ Solutiontask8/Program.cs
@@ -0,0 +1,14 @@
+﻿string? A = Console.ReadLine();//Задача 8
+if(A != null)
+{
+    int numberA = int.Parse (A);
+    int numberB = 2;
+    int count = 0;
+    while(numberB < numberA)
+    {
+        count = count + numberB;
+        numberB = numberB + 2;
+    }
+   
+        Console.WriteLine(count);
+}
