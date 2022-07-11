208ba034451eb60a2805836e60cecbe866c3bc8e
Anton <seemannRR@ya.ru>
2022-07-11 20:57:03 +0300

    add Task4

---
+++ Solutiontask4/Program.cs
@@ -0,0 +1,17 @@
+﻿// string? inputline = Console.ReadLine();
+
+// if(inputline != null)
+// {
+//     int inputnumber = int.Parse (inputline);
+    
+//     int outputnumber = inputnumber%10; 
+
+//     Console.WriteLine(outputnumber);
+// }
+string? inputline = Console.ReadLine();// через массив
+
+if(inputline != null)
+{
+    char[] M = inputline.ToCharArray ();
+    Console.WriteLine(M[2]);
+}
