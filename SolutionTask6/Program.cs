7b09fc6209bc32e8fc8c76bfa9abd492efa0abad
Anton <seemannRR@ya.ru>
2022-07-11 21:40:47 +0300

    add Task6

---
+++ SolutionTask6/Program.cs
@@ -0,0 +1,31 @@
+﻿string? A = Console.ReadLine();//Задача 4
+string? B = Console.ReadLine();
+string? C = Console.ReadLine();
+if(A != null && B != null && C != null)
+{
+    int numberA = int.Parse (A);
+    int numberB = int.Parse (B);
+    int numberC = int.Parse (C);
+    if(numberA >= numberB)
+    {
+        if (numberA >= numberC)
+        {
+          Console.WriteLine(numberA);  
+        }
+        else 
+        {
+         Console.WriteLine(numberC);   
+        }
+    }
+    else
+    {
+        if (numberB >= numberC)
+        {
+          Console.WriteLine(numberB);  
+        }
+        else 
+        {
+         Console.WriteLine(numberC);   
+        }
+    }
+}
