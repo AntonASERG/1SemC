6eed2e28be186e291b5b585303b7de4fc3c1e5d3
Anton <seemannRR@ya.ru>
2022-07-11 21:51:56 +0300

    add Task7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,14 @@
+﻿string? A = Console.ReadLine();//Задача 6
+if(A != null)
+{
+    int numberA = int.Parse (A);
+    int numberB = numberA%2;
+    if(numberB == 0)
+    {
+        Console.WriteLine("четное");
+    }
+    else
+    {
+        Console.WriteLine("нечетное");
+    }
+}
