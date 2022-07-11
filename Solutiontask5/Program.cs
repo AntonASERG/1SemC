9cab22a58a0b555e2515249e6b7a367b2bf581c1
Anton <seemannRR@ya.ru>
2022-07-11 21:19:30 +0300

    add Task5

---
+++ Solutiontask5/Program.cs
@@ -0,0 +1,14 @@
+﻿string? A = Console.ReadLine();//Задача 2
+if(A != null && B != null)
+{
+    int numberA = int.Parse (A);
+    int numberB = int.Parse (B);
+    if(numberA >= numberB)
+    {
+        Console.WriteLine(numberA);
+    }
+    else
+    {
+        Console.WriteLine(numberB);
+    }
+}
