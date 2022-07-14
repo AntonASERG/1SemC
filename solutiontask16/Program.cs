aeb679b6cf8465a9787d957d665da9601c68b164
Anton <seemannRR@ya.ru>
2022-07-14 23:51:53 +0300

    sol1 16

---
+++ solutiontask16/Program.cs
@@ -0,0 +1,21 @@
+﻿//задача X=Y*Y ?
+
+Console.Write("X: ");
+string? x = Console.ReadLine();// ? не допускает Null
+Console.Write("Y: ");
+string? y = Console.ReadLine();
+
+if(x != null && y != null )
+{
+    int xDigit = int.Parse (x);//ОЧЕНЬ ВАЖНО Parse - перевод в int!!
+    int yDigit = int.Parse (y);
+    if(xDigit == yDigit*yDigit)
+    {
+        Console.WriteLine("Да");  
+    }
+    else
+    {
+      Console.WriteLine("Нет");  
+    }
+    
+}
