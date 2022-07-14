682d64469b093bcfe58a3de84bb2af955a4299cd
Anton <seemannRR@ya.ru>
2022-07-14 23:35:10 +0300

    sol14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,20 @@
+﻿//задача 14 X кратно ли 7 & 23 
+
+Console.Write("X: ");
+string? x = Console.ReadLine();// ? не допускает Null
+
+if(x != null)
+{
+    int xDigit = int.Parse (x);//ОЧЕНЬ ВАЖНО Parse - перевод в int!!
+    int y = xDigit%7;
+    int z = xDigit%23;
+    if(z == 0 && y == 0) //&& -u, || - или
+    {
+        Console.WriteLine("Кратно");  
+    }
+    else
+    {
+      Console.WriteLine("Некратно"); 
+    }
+    
+}
