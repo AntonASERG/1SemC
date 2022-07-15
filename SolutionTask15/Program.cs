528bfbd1a20096b03e6e50d4ebcd5194e05cdfe1
Anton <seemannRR@ya.ru>
2022-07-15 14:42:17 +0300

    sol15

---
+++ SolutionTask15/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 15 Дни недели
+Console.Write("Day of week:  ");
+string? x = Console.ReadLine();
+
+if(x != null)
+{
+    int d = int.Parse (x);
+    if((d == 6) || (d == 7))
+    {
+        Console.WriteLine("Выходной");  
+    }
+    else
+    {
+        if (d >0 && d < 6)
+        {
+            Console.WriteLine("Будний"); 
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не то, что надо");
+        }
+         
+    } 
+}
