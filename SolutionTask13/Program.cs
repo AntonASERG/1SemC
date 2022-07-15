1f3863f21d393caf2fbfdd7c48c7058520ea904c
Anton <seemannRR@ya.ru>
2022-07-15 14:20:11 +0300

    sol13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10 ABC->B
+string? x = Console.ReadLine();
+
+if(x != null)
+{
+    char [] arr = x.ToCharArray();
+    int length = arr.Length;  
+        if(length > 3)
+        {
+            Console.WriteLine(arr[2]);  
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет"); 
+        } 
+}
