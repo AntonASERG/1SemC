4691ad42eba5f500d0df4efceaa18bf8f22ffc71
Anton <seemannRR@ya.ru>
2022-07-15 13:54:36 +0300

    sol10dz

---
+++ SolutionTask10DZ/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 10 ABC->B
+string? x = Console.ReadLine();
+void VAR1()
+{
+if(x != null)
+    {
+        int xDigit = int.Parse (x);//ОЧЕНЬ ВАЖНО Parse - перевод в int!!
+        if(xDigit > 99 && xDigit < 1000)
+        {
+            int yDigit = xDigit%100;
+            int zDigit = yDigit/10;
+            Console.WriteLine(zDigit);  
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не трехзначное число"); 
+        } 
+    }
+}  
+
+void VAR2()//Варту
+{
+    if(x != null)
+    {
+    string sn = x.ToString();
+    int xDi = int.Parse (x);
+        if(xDi > 99 && xDi < 1000)
+        {
+            Console.WriteLine(sn[1]);  
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не трехзначное число"); 
+        } 
+    }
+}
+
+VAR1();
+VAR2();
