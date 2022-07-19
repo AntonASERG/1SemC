c98fb95d79c0a0d9236191f3825d573bdaf2bab9
Anton <seemannRR@ya.ru>
2022-07-19 11:39:50 +0300

    sol18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных
+// координат точек в этой четверти (x и y).
+
+//Задача 18
+
+void printA(int number)// НИЧЕГО НЕ ВОЗВРАЩАЕТ на(перемен) не обращаю внимание
+{
+    if(number == 1) Console.WriteLine("Допустимо X>0 Y>0");
+    if(number == 2) Console.WriteLine("Допустимо X<0 Y>0");
+    if(number == 3) Console.WriteLine("Допустимо X<0 Y<0");
+    if(number == 4) Console.WriteLine("Допустимо X>0 Y<0");
+}
+
+string? inputline = Console.ReadLine();
+
+if(inputline != null)
+{
+    int inputnumber = int.Parse (inputline);
+    printA(inputnumber);
+}
