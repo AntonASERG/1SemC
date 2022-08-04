e9f11cb836a03802f601c99c363de4b5c08a1d09
Anton <seemannRR@ya.ru>
2022-08-04 22:06:19 +0300

    sol26_28

---
+++ SolutionTask26/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 26 количество цифр в числе
+
+Console.WriteLine("Введите число");
+
+string inputLineNumber = Console.ReadLine() ?? "";
+int inputnumber = int.Parse(inputLineNumber);
+
+DateTime timePoint = DateTime.Now;//Задаем переменную Таймпоинт
+
+int VariantChar()
+{
+    // int numberLength = 0;
+
+    // char[] array = inputLineNumber.ToCharArray(); //Преобразование в массив строки
+    // numberLength = array.Length;
+    // return numberLength;
+    return inputLineNumber.ToCharArray().Length;//!!!То же самое только короче - строка в массив и вызвать длину
+}
+
+int WariantSimle()
+{
+   int numberl = 0;
+   int digits = 1; 
+   while(digits < inputnumber)
+   {
+    digits=digits*10;
+    numberl++;
+   }
+return numberl;
+}
+
+int VariantLog()
+{
+    return (int)Math.Log10(inputnumber)+1;//!!!!Принудительное возвращение типов
+}
+
+int result = 0;
+
+timePoint = DateTime.Now;//Задаем исх Таймпоинт
+result = VariantChar();
+Console.WriteLine(result);
+Console.WriteLine( DateTime.Now - timePoint);//Таймпоинт выполнения
+
+timePoint = DateTime.Now;//Задаем исх Таймпоинт
+result = WariantSimle();
+Console.WriteLine(result);
+Console.WriteLine( DateTime.Now - timePoint);//Таймпоинт выполнения
+
+timePoint = DateTime.Now;//Задаем исх Таймпоинт
+result = VariantLog();
+Console.WriteLine(result);
+Console.WriteLine( DateTime.Now - timePoint);//Таймпоинт выполнения
+
+// для больших чисел меняем переменную int (4 байта) на long (8 байт ) и т д
---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28 Произведение всех чисел от 1 до Числа N
+
+Console.WriteLine("Введите число");
+
+int inputnumber = int.Parse(Console.ReadLine());
+
+void VariantSimple()
+{
+    int sumOfNumbers = 1;
+    for(int i =1; i<=inputnumber; i++)
+    {
+        sumOfNumbers *= i; //sumOfNumbers = sumOfNumbers * i
+    } 
+    Console.WriteLine("Произведение чисел от 1 до " + inputnumber + " = " + sumOfNumbers);
+}
+
+VariantSimple();
