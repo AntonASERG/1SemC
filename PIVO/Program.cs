5141f80622e4211caf8a8b1fca4bae5bc9a1cbfa
Anton Anisimov <seemannrr@ya.ru>
2022-08-06 23:20:49 +0300

    Piv

---
+++ PIVO/Program.cs
@@ -0,0 +1,8 @@
+﻿// За пивом - неудачно - не могу понять как :(
+System.Random numberSint = new Random();
+Console.WriteLine("Введите имена: ");
+string? names = Console.ReadLine();
+char[]array = names.ToCharArray();
+int len = array.Length;
+int i = numberSint.Next(0,len);
+Console.WriteLine(array[i]);
