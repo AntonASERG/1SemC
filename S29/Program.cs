6cd6f6da2aab4a1ad635e1b698ba3f7e357752d6
Anton Anisimov <seemannrr@ya.ru>
2022-08-06 22:47:39 +0300

    s29

---
+++ S29/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29 массив из 8 элементов *
+System.Random numberSint = new Random(); // Обращение к системному рандомайзеру
+Console.WriteLine("Введите длину массива:");
+int len = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начало диапазона:");
+int start = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конец диапазона:");
+int end = int.Parse(Console.ReadLine());
+
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i<len-1)
+    {
+        Console.Write(numberSint.Next(start,end+1)+",");// Диапазон от 0 до 1 вкл
+        i++;
+    }
+    Console.Write(numberSint.Next(start,end+1));
+    Console.Write("]");
+}
+
+VariantNaive();
