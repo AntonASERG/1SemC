a1cae3470cae74a819a3e6761177057214d68eb8
Anton <seemannRR@ya.ru>
2022-08-08 19:02:30 +0300

    Pivas

--- PIVO/Program.cs
+++ PIVO/Program.cs
@@ -1,8 +1,8 @@
 ﻿// За пивом - неудачно - не могу понять как :(
 System.Random numberSint = new Random();
 Console.WriteLine("Введите имена: ");
-string? names = Console.ReadLine();
-char[]array = names.ToCharArray();
-int len = array.Length;
+string? inputLineNames = Console.ReadLine();
+string[] names = inputLineNames.Split(new char[] { ',' });
+int len = names.Length;
 int i = numberSint.Next(0,len);
-Console.WriteLine(array[i]);
+Console.WriteLine(names[i]);
