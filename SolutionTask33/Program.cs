ada0960b4e4a9bfca8929125c9145829dc01b3a0
Anton <seemannRR@ya.ru>
2022-08-09 00:01:31 +0300

    s32 33 35

---
+++ SolutionTask33/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 33 усть ли число в массиве
+
+int[] FillingArray() //метод задать массив
+{
+    int[] outArray = new int[12]; 
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < 12)
+    {
+        outArray[i] = numberSintezator.Next(-9, 10);
+        i++;
+    }
+    return outArray;
+}
+
+void PrintArray(int[] array)
+{// метод распечатывания массива
+    //Буфферная переменная
+    int i = 0;
+    while (i < array.Length - 1)// Пробежать по всем элементам массива
+    {
+        Console.Write(array[i] + ",");
+        i++;
+    }
+    Console.WriteLine(array[i]);//Допечатать последний элемент массива
+}
+
+bool ColculateTask(int[] array, int searchNumber)// Да или нет
+{
+    bool resultSearch = false;// по умолчанию
+    int i = 0;// итератор
+    while (i<12)
+    {
+        if (array[i] == searchNumber)// принимает Буул переменную
+        {
+            resultSearch = true;
+            break;// остановит цикл если нужно 1 значение
+        }
+        else
+        {
+            resultSearch = false;
+        }
+        i++;
+    }
+    return resultSearch;
+}
+
+int [] bufferArray = FillingArray();
+PrintArray(bufferArray);
+Console.WriteLine("Введите число для поиска");
+int searchNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(ColculateTask(bufferArray,searchNumber));
---
+++ SolutionTask35/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 35 массив 123эл., сколько эл. в отрезке 10, 99
+
+int[] FillingArray() //Задаем массив на 123
+{
+    int[] outArray = new int[123]; 
+    int i = 0;
+    System.Random numberSintezator = new System.Random();
+    while (i < 123)
+    {
+        outArray[i] = numberSintezator.Next(0, 123);
+        i++;
+    }
+    return outArray;
+}
+
+void PrintArray(int[] array)// выводим на экран массив из 123 случ чисел
+{
+    int i = 0;
+    while (i < array.Length - 1)// Пробежать по всем элементам массива
+    {
+        Console.Write(array[i] + ",");
+        i++;
+    }
+    Console.WriteLine(array[i]);//Допечатать последний элемент массива
+}
+
+int ColculateTask(int[] array, int totalSum)// Считаем сумму эл.в
+{
+    int sum = 0;
+    int i = 0;// итератор
+    while (i<123)
+    {
+        if (array[i] > 9 && array[i] <100)// принимает Буул переменную
+        {
+            sum = sum+array[i];
+        }
+        else
+        {
+            sum = sum+0;
+        }
+        i++;
+    }
+    return sum;
+}
+
+int [] bufferArray = FillingArray();
+PrintArray(bufferArray);
+Console.Write("Сумма = ");
+int totalSum = 0;
+Console.WriteLine(ColculateTask(bufferArray,totalSum));
