5be8449ff9f2e0671c3158ba1c5cd11eaf87db01
Anton <seemannRR@ya.ru>
2022-08-12 14:51:24 +0300

    2330

---
+++ SolutionTask42/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 42 перевод из десятеричной в двоичную
+Console.WriteLine(Convert.ToString(int.Parse(Console.ReadLine()),2));
+
+int number = int.Parse(Console.ReadLine());
+string outline = Convert.ToString(number,2);// метод в Тустринге - 2 система счисления
+Console.WriteLine(outline);
+
+
+// Строчки разбираются изнутри!!!
+//Расписать  обычный алгоритм - он быстрее!!
