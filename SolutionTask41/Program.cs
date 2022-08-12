97707b7d951486933a41ca27440b63566c4210bc
Anton <seemannRR@ya.ru>
2022-08-12 21:38:35 +0300

    41

---
+++ SolutionTask41/Program.cs
@@ -0,0 +1,46 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+//======================================================================================
+Console.Write("Задайте размер массива - ");
+int m = int.Parse(Console.ReadLine());
+int[] num = new int [m];
+
+int [] total = Read(m);
+int res = Calculate(total);
+Print(res);
+
+
+
+int[] Read (int m)// метод, чтобы пользователь ввел числа
+{
+int[] buffer = new int [m];
+for (int i = 0; i < m; i++)
+{
+    buffer[i] = int.Parse(Console.ReadLine());
+}
+return buffer;
+}
+
+int Key(int[] buffer)
+{
+    ConsoleKeyInfo key;
+    key = Console.ReadKey();
+    if ((key.key == NumPad-) && (key.key == D-)  )
+}
+
+int Calculate (int[] buffer)
+{
+    int b =0;
+    for (int i =0; i < buffer.Length; i++)
+    {
+        if (buffer[i] > 0)
+        {
+            b++;
+        }
+    }
+    return b;
+}
+
+void Print (int b)
+{
+    Console.WriteLine("больше нуля   "+b+"   чисел");
+}
